4f4fbccfacd15bcefc1005c115509c7efa3909ba
かがみ <kagami@kagami.me>
2024-08-09 17:34:55 +0800

    fix: radio indicator alignment

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -28,19 +28,19 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        'aspect-square h-4 w-4 rounded-full border-2 border-input bg-primary-foreground text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+        'relative aspect-square h-4 w-4 rounded-full border-2 border-input bg-primary-foreground text-primary focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
         className,
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+      <RadioGroupPrimitive.Indicator>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
           viewBox="0 0 16 16"
           fill="currentColor"
-          className="lucide lucide-circle h-[0.8em] w-[0.8em] fill-[#ff6900]"
+          className="absolute h-[16px] w-[16px] -translate-y-1/2 fill-[#ff6900]"
         >
           <circle cx="8" cy="8" r="6" />
         </svg>
