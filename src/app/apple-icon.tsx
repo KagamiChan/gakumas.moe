b53d49dd53da35fa00924b7f4068016bac3b8672
かがみ <kagami@kagami.me>
2024-08-11 14:08:37 +0800

    fix: ios icon

--- src/app/apple-icon.tsx
+++ src/app/apple-icon.tsx
@@ -1,4 +1,4 @@
-import { ImageResponse } from 'next/og'
+import Icon from './icon'
 
 export const size = {
   width: 256,
@@ -6,21 +6,4 @@ export const size = {
 }
 export const contentType = 'image/png'
 
-export default function Icon() {
-  return new ImageResponse(
-    (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="256"
-        height="256"
-        viewBox="0 0 67.7 67.7"
-      >
-        <rect width="100%" height="100%" fill="#ffffff" />
-        <path fill="#f39800" d="M8.5 8.5h50.8v50.8H8.5z" />
-      </svg>
-    ),
-    {
-      ...size,
-    },
-  )
-}
+export default Icon
