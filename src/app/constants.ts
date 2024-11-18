0d158c68664821e144368e92fb59b17029d90920
かがみ <kagami@kagami.me>
2024-11-19 03:16:07 +0800

    feat: seo improve (#92)

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,5 @@
 export const APP_NAME = '学マス計算機α'
 export const APP_DEFAULT_TITLE = '学マス計算機α by 初星学園サポート科'
 export const APP_TITLE_TEMPLATE = '%s - App版'
-export const APP_DESCRIPTION = '学園アイドルマスター非公式アシスタント!'
+export const APP_DESCRIPTION =
+  '学園アイドルマスター（学マス）非公式計算機。Gakuen Idolmaster (Gakumas) unofficial calculator.'
